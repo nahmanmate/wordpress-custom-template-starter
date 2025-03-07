1d2726ec01c82ac7be3c7015089d70bf982b199f
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-07 15:30:03 +1100

    feat: added dynamic post content loading

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <h1>Hello World!</h1>
+
+<?php
+    while(have_posts()) {
+        the_post(); ?>
+        <div>
+            <h2><a href="<?php the_permalink() ?>"> <?php the_title ?></a> </h2>
+        </div>
+        <?php }
+?>
+
+<p>Footer</p>
